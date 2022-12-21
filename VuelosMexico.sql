61822555ebcffd55fddda41d6eadb4511efcb631
EusaHack <eusa510@gmail.com>
2022-12-21 16:57:43 -0600

    Update VuelosMexico.sql

--- VuelosMexico.sql
+++ VuelosMexico.sql
@@ -60,25 +60,51 @@ select * from aeropuertos;
 select * from movimientos;
 select * from vuelos;
 
---¿Cuál es el nombre aeropuerto que ha tenido mayor movimiento durante el año?--
+
+--Â¿CuÃ¡l es el nombre aeropuerto que ha tenido mayor movimiento durante el aÃ±o?--
+--se asigna un alias con letras iniciales de las tablas
+--se selecciona las tablas correspondientes y en especial uno que se cuentan los valores 
+--se sobre escribe el nombre de la columna por : MovimientoVuelos
+--se buscan coincidencias entre las tablas con inner join
 select a.nombre_aeropuerto, count(v.id_movimiento)'MovimientoVuelos' from aeropuertos a
 inner join vuelos v
+-- se hace referencia de a.id_aeropuerto a v.id_aeropuerto para que tome los valores 
 on a.id_aeropuerto = v.id_aeropuerto
+-- se combinan los registros con valores identicos 
 group by a.nombre_aeropuerto
+-- los valores se ordenan de forma descendente 
 order by MovimientoVuelos desc;
---¿Cuál es el nombre aerolínea que ha realizado mayor número de vuelos durante el año?--
+--Â¿CuÃ¡l es el nombre aerolÃ­nea que ha realizado mayor nÃºmero de vuelos durante el aÃ±o?--
+--se asigna un alias con letras iniciales de las tablas
+--se selecciona las tablas correspondientes y en especial uno que se cuentan los valores 
+--se sobre escribe el nombre de la columna por : MovimientoAerolinea
+--se buscan coincidencias entre las tablas con inner join
 select a.nombre_aerolinea, count(v.id_movimiento)'MovimientoAerolinea' from aerolineas a
 inner join vuelos v
+-- se hace referencia de a.id_aerolinea = v.id_aerolinea para que tome los valores
 on a.id_aerolinea = v.id_aerolinea
+-- se combinan los registros con valores identicos 
 group by a.nombre_aerolinea
+-- los valores se ordenan de forma descendente 
 order by MovimientoAerolinea desc;
---¿En qué día se han tenido mayor número de vuelos?--
+--Â¿En quÃ© dÃ­a se han tenido mayor nÃºmero de vuelos?--
+--se selcciona el valor dia y el valor de movimientos se va contando
+--se sobre escribe el nombre de la columna por : Vuelos
 select dia , count(id_movimiento) 'Vuelos' from vuelos 
+-- se combinan los registros con valores identicos 
 group by dia
+-- los valores se ordenan de forma descendente 
 order by Vuelos desc;
---¿Cuáles son las aerolíneas que tienen mas de 2 vuelos por día?--
+--Â¿CuÃ¡les son las aerolÃ­neas que tienen mas de 2 vuelos por dÃ­a?--
+--se asigna un alias con letras iniciales de las tablas
+--se selecciona las tablas correspondientes y en especial uno que se cuentan los valores 
+--se sobre escribe el nombre de la columna por : MovimientoVuelos
+--se buscan coincidencias entre las tablas con inner join
 select a.nombre_aerolinea, count(v.id_movimiento)'MovimientoAerolinea'  from aerolineas a
 inner join vuelos v
+-- se hace referencia de a.id_aerolinea = v.id_aerolinea para que tome los valores
 on a.id_aerolinea = v.id_aerolinea 
+-- se combinan los registros con valores identicos
 group by a.nombre_aerolinea 
-having COUNT(v.id_movimiento) > 2;
+-- se muestran los registros agrupados que son mayores a  2 
+having count(v.id_movimiento) > 2;
